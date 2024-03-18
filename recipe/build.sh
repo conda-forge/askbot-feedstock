86c292dc9341d946f97210ea8a6593b385fab248
Uwe L. Korn <uwe.korn@quantco.com>
2024-03-18 19:13:18 +0100

    Re-add Python install

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+${PYTHON} -m pip install . -vv
+
 if [[ "${target_platform}" == "osx-arm64" ]]; then
     export npm_config_arch="arm64"
 fi
@@ -9,15 +11,9 @@ fi
 # Don't use pre-built gyp packages
 export npm_config_build_from_source=true
 
-rm $PREFIX/bin/node
-ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
-
 NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
-# install askbot globally from the npm registry
-# all things coming after this are just concerned with generating the ThirdPartyLicenses.txt file
-npm install -g ${PKG_NAME}@${PKG_VERSION}
-
+pushd askbot/media
 pnpm import
 pnpm install --prod
 pnpm licenses list --json | pnpm-licenses generate-disclaimer --json-input --output-file=ThirdPartyLicenses.txt
